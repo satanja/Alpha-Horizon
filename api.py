a70d57e96c6585ead888b92bdae88fffc8181a19
satanja <s.a.tanja@student.tue.nl>
2019-06-01 16:02:31 +0200

    Add API parser

---
+++ api.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from match import Match
+
+UPCOMING = 'https://alpha.tl/api?upcomingmatches'
+MATCH_PREFIX = 'https://alpha.tl/api?match=' 
+
+def get_upcoming():
+    
+    response = requests.get(UPCOMING)
+    upcoming_matches = json.loads(response.text)
+
+    id = None
+
+    for match in upcoming_matches:
+        if match["team1"]["id"] == 61 or match["team2"]["id"] == 61:
+            id = match["id"]
+    
+
+    match_url = MATCH_PREFIX + str(id)
+    response = requests.get(match_url)
+    upcoming_match = json.loads(response.text)
+
+    channel = upcoming_match["channel"]
+    team1 = upcoming_match["team1"]["name"]
+    team2 = upcoming_match["team2"]["name"]
+    maps = upcoming_match["maps"]
+
+    result = Match(id, channel, team1, team2, maps)
+
+    # print(upcoming_match)
+    return result
+    
---
+++ match.py
@@ -0,0 +1,8 @@
+class Match:
+    def __init__(self, id = 0, channel = 0, team1 = "", team2 = "", maps = []):
+        self.id = id
+        self.channel = channel
+        self.team1 = team1
+        self.team2 = team2 
+        self.maps = maps
+        
